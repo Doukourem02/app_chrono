a71758698ec57ddc270d3736fa060bdd744e472f
Doukourem02 <mohameddoukoure340@gmail.com>
2026-04-14 22:18:23 +0000

    message de mise à jour

--- app_chrono/app.config.js
+++ app_chrono/app.config.js
@@ -19,7 +19,7 @@ module.exports = {
     ios: {
       bundleIdentifier: "com.anonymous.app-chrono",
       /** À incrémenter (+1) avant chaque build TestFlight / App Store (autoIncrement incompatible avec app.config.js). */
-      buildNumber: "49",
+      buildNumber: "50",
       supportsTablet: true,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
