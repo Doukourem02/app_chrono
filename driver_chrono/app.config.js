dda43111c38488c60af7f06d1450e745117e7136
Doukourem02 <mohameddoukoure340@gmail.com>
2026-05-09 19:43:40 +0000

    message de mise à jour

--- driver_chrono/app.config.js
+++ driver_chrono/app.config.js
@@ -19,7 +19,7 @@ module.exports = {
     ios: {
       bundleIdentifier: "com.anonymous.driver-chrono",
       /** À incrémenter (+1) avant chaque build TestFlight / App Store (autoIncrement incompatible avec app.config.js). */
-      buildNumber: "139",
+      buildNumber: "140",
       supportsTablet: true,
       infoPlist: {
         CFBundleDisplayName: "Krono pro",
@@ -28,7 +28,7 @@ module.exports = {
     },
     android: {
       package: "com.anonymous.driver_chrono",
-      versionCode: 139,
+      versionCode: 140,
       adaptiveIcon: {
         backgroundColor: "#FFFFFF",
         foregroundImage: "./assets/images/logo/LOGO_APP1.png",
